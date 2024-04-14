730b92f94d378d353331e8da3de742c4e25f9f28
BriansTjipto <brianstjipto@gmail.com>
2024-04-14 22:33:24 +0700

    Add support for https://tttapp.vercel.app in CORS origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:5173", "https://tttapp.vercel.app"],
     credentials: true
   })
 )
--- index_prod.js
+++ index_prod.js
@@ -1,77 +1,402 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const { MongoClient } = require("mongodb")
+require("dotenv").config()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:8000"],
-    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:5173",
+      "https://tttapp.vercel.app"
+    ],
+    credentials: true
   })
-);
+)
 
 app.get("/", async (req, res) => {
   try {
-    res.status(200).json("Welcome to the Envogue API");
+    res.status(200).json("Welcome to the TicTacToe API")
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+    console.error(error)
+    res.status(500).json({ message: error.message })
   }
-});
+})
 
 app.post("/new-game", async (req, res) => {
   try {
     let client = await MongoClient.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true,
-    });
-    const database = client.db("account");
-    const collection = database.collection("spa_user");
-
-    if (
-      req.body.username === process.env.ENVOGUEADMINSSID &&
-      req.body.password === process.env.ENVOGUEADMINPASSWORD
-    ) {
-      return res.status(200).json({
-        name: "envogueadmin",
-        spa_id: "admin",
-        token: "adminnotoken",
-      });
+      useUnifiedTopology: true
+    })
+    const {
+      size,
+      disappearing,
+      disappearing_rounds,
+      player_0,
+      player_1,
+      player_0_name,
+      player_1_name
+    } = req.body
+    if (size < 3) {
+      return res.status(400).json({ message: "Grid size must be 3 or more." })
+    }
+    if (disappearing !== true && disappearing !== false) {
+      return res
+        .status(400)
+        .json({ message: "Disappearing must be true or false." })
+    }
+    if (disappearing && disappearing_rounds < 1) {
+      return res
+        .status(400)
+        .json({ message: "Disappearing rounds must be 1 or more." })
+    }
+    if (disappearing === false && disappearing_rounds > 0) {
+      return res.status(400).json({
+        message: "Disappearing rounds must be 0 if disappearing is false."
+      })
+    }
+    if (player_0 === player_1) {
+      return res
+        .status(400)
+        .json({ message: "Player 0 and player 1 must be different." })
     }
 
-    const user = await collection.findOne({ username: req.body.username });
+    const database = client.db("ttt")
+    const collection = database.collection("game")
 
-    if (!user) {
-      return res.status(400).json("Cannot find user");
+    const gameCode = generateGameCode()
+    const newGame = {
+      gameCode,
+      size,
+      board: Array(size * size).fill(null),
+      currentPlayer: "1",
+      winner: null,
+      winningLine: [],
+      player_0,
+      player_0_name: player_0_name ? player_0_name : "",
+      player_1,
+      player_1_name: player_1_name ? player_1_name : "",
+      disappearing,
+      disappearing_rounds,
+      rounds: []
     }
 
-    if (
-      await bcrypt.compare(req.body.password + process.env.SALT, user.password)
-    ) {
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRATION_TIME,
-      });
+    await collection.insertOne(newGame)
 
-      await collection.updateOne({ _id: user._id }, { $set: { token: token } });
-
-      res.status(200).json({
-        token: token,
-        spa_id: user.spa_id,
-        name: user.name,
-      });
-    } else {
-      res.status(401).json("Invalid password");
+    const board = []
+    for (let i = 0; i < newGame.size; i++) {
+      board.push(newGame.board.slice(i * newGame.size, (i + 1) * newGame.size))
     }
+
+    res.status(200).json({
+      gameCode: newGame.gameCode,
+      size: newGame.size,
+      board: board,
+      currentPlayer: newGame.currentPlayer,
+      winner: newGame.winner,
+      winningLine: [],
+      player_0: newGame.player_0,
+      player_0_name: newGame.player_0_name ? newGame.player_0_name : "",
+      player_1: newGame.player_1,
+      player_1_name: newGame.player_1_name ? newGame.player_1_name : "",
+      disappearing: newGame.disappearing,
+      disappearing_rounds: newGame.disappearing_rounds,
+      rounds: newGame.rounds
+    })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+    console.error(error)
+    res.status(500).json({ message: error.message })
   } finally {
     if (client) {
-      client.close();
+      client.close()
     }
   }
-});
+})
 
-module.exports = app;
+app.post("/join-game", async (req, res) => {
+  try {
+    let client = await MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true
+    })
+    const { gameCode, player0, player1 } = req.body
+
+    if (!gameCode) {
+      return res.status(400).json({ message: "Missing game code" })
+    }
+
+    const database = client.db("ttt")
+    const collection = database.collection("game")
+
+    const game = await collection.findOne({ gameCode })
+    if (!game) {
+      return res.status(404).json({ message: "Game not found." })
+    }
+    if (game.player_0name !== "" && game.player_1_name !== "") {
+      return res.status(400).json({ message: "Game is full." })
+    }
+    if (player0 === player1) {
+      return res
+        .status(400)
+        .json({ message: "Player 0 and player 1 must be different." })
+    }
+    if (game.player_0_name && game.player_0_name === player1) {
+      return res
+        .status(400)
+        .json({ message: "This name is already taken by Player 1." })
+    } else if (game.player_1_name && game.player_1_name === player0) {
+      return res
+        .status(400)
+        .json({ message: "This name is already taken by Player 0." })
+    }
+
+    await collection.updateOne(
+      { gameCode },
+      {
+        $set: {
+          player_0_name: player0 || "",
+          player_1_name: player1 || ""
+        }
+      }
+    )
+
+    const editedGame = await collection.findOne({ gameCode })
+
+    res
+      .status(200)
+      .json({ message: "Player names set successfully.", editedGame })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message })
+  } finally {
+    if (client) {
+      client.close()
+    }
+  }
+})
+
+app.get("/game/:gameCode", async (req, res) => {
+  try {
+    let client = await MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true
+    })
+    const { gameCode } = req.params
+    if (gameCode.length !== 10 || gameCode === "" || gameCode === null) {
+      return res.status(400).json({ message: "Invalid game code." })
+    }
+
+    const database = client.db("ttt")
+    const collection = database.collection("game")
+
+    const game = await collection.findOne({ gameCode })
+    if (!game) {
+      return res.status(404).json({ message: "Game not found." })
+    }
+
+    const board = []
+    for (let i = 0; i < game.size; i++) {
+      board.push(game.board.slice(i * game.size, (i + 1) * game.size))
+    }
+
+    res.status(200).json({
+      gameCode: game.gameCode,
+      size: game.size,
+      board: board,
+      currentPlayer: game.currentPlayer,
+      winner: game.winner,
+      winningLine: game.winningLine,
+      player_0: game.player_0,
+      player_0_name: game.player_0_name,
+      player_1: game.player_1,
+      player_1_name: game.player_1_name,
+      disappearing: game.disappearing,
+      disappearing_rounds: game.disappearing_rounds,
+      rounds: game.rounds
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message })
+  } finally {
+    if (client) {
+      client.close()
+    }
+  }
+})
+
+app.post("/play", async (req, res) => {
+  try {
+    let client = await MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true
+    })
+    const { gameCode, row, col } = req.body
+    if (row < 0 || col < 0) {
+      return res.status(400).json({ message: "Invalid move." })
+    }
+    if (gameCode.length !== 10 && gameCode === "" && gameCode === null) {
+      return res.status(400).json({ message: "Invalid game code." })
+    }
+
+    const database = client.db("ttt")
+    const collection = database.collection("game")
+
+    const game = await collection.findOne({ gameCode })
+    if (!game) {
+      return res.status(404).json({ message: "Game not found." })
+    }
+
+    if (!game.player_0_name || !game.player_1_name) {
+      return res.status(400).json({ message: "Game not ready." })
+    }
+
+    const index = row * game.size + col
+    if (game.board[index] !== null) {
+      return res.status(400).json({ message: "Invalid move." })
+    }
+
+    game.rounds.push({ row, col, player: game.currentPlayer })
+
+    if (game.disappearing && game.rounds.length > game.disappearing_rounds) {
+      const disappearIndex = game.rounds.length - game.disappearing_rounds - 1
+      const { row: disappearRow, col: disappearCol } =
+        game.rounds[disappearIndex]
+      const disappearIndexBoard = disappearRow * game.size + disappearCol
+      game.board[disappearIndexBoard] = null
+    }
+
+    game.board[index] = game.currentPlayer
+    game.currentPlayer = game.currentPlayer === "1" ? "0" : "1"
+
+    const winner = checkForWin(game.board, game.size, row, col)
+    if (winner) {
+      game.winner = winner
+      game.winningLine = getWinningLine(winner, row, col, game.size, game)
+    }
+
+    await collection.updateOne({ gameCode }, { $set: game })
+
+    const board = []
+    for (let i = 0; i < game.size; i++) {
+      board.push(game.board.slice(i * game.size, (i + 1) * game.size))
+    }
+
+    res.status(200).json({
+      gameCode: game.gameCode,
+      size: game.size,
+      board: board,
+      currentPlayer: game.currentPlayer,
+      winner: game.winner,
+      winningLine: game.winningLine,
+      player_0: game.player_0,
+      player_0_name: game.player_0_name,
+      player_1: game.player_1,
+      player_1_name: game.player_1_name,
+      disappearing: game.disappearing,
+      disappearing_rounds: game.disappearing_rounds,
+      rounds: game.rounds
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: error.message })
+  } finally {
+    if (client) {
+      client.close()
+    }
+  }
+})
+
+function generateGameCode() {
+  let code = ""
+  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  for (let i = 0; i < 10; i++) {
+    code += possibleChars.charAt(
+      Math.floor(Math.random() * possibleChars.length)
+    )
+  }
+
+  return code
+}
+
+function checkForWin(board, size, lastRow, lastCol) {
+  const player = board[lastRow * size + lastCol]
+
+  for (let i = 0; i < size; i++) {
+    if (board[lastRow * size + i] !== player) break
+    if (i === size - 1) return player
+  }
+
+  for (let i = 0; i < size; i++) {
+    if (board[i * size + lastCol] !== player) break
+    if (i === size - 1) return player
+  }
+
+  if (lastRow === lastCol) {
+    for (let i = 0; i < size; i++) {
+      if (board[i * size + i] !== player) break
+      if (i === size - 1) return player
+    }
+  }
+
+  if (lastRow + lastCol === size - 1) {
+    for (let i = 0; i < size; i++) {
+      if (board[i * size + (size - 1 - i)] !== player) break
+      if (i === size - 1) return player
+    }
+  }
+
+  return null
+}
+
+function getWinningLine(player, lastRow, lastCol, size, game) {
+  const line = []
+
+  for (let i = 0; i < size; i++) {
+    if (game.board[lastRow * size + i] !== player) break
+    if (i === size - 1) {
+      for (let j = 0; j < size; j++) {
+        line.push({ row: lastRow, col: j })
+      }
+      return line
+    }
+  }
+
+  for (let i = 0; i < size; i++) {
+    if (game.board[i * size + lastCol] !== player) break
+    if (i === size - 1) {
+      for (let j = 0; j < size; j++) {
+        line.push({ row: j, col: lastCol })
+      }
+      return line
+    }
+  }
+
+  if (lastRow === lastCol) {
+    for (let i = 0; i < size; i++) {
+      if (game.board[i * size + i] !== player) break
+      if (i === size - 1) {
+        for (let j = 0; j < size; j++) {
+          line.push({ row: j, col: j })
+        }
+        return line
+      }
+    }
+  }
+
+  if (lastRow + lastCol === size - 1) {
+    for (let i = 0; i < size; i++) {
+      if (game.board[i * size + (size - 1 - i)] !== player) break
+      if (i === size - 1) {
+        for (let j = 0; j < size; j++) {
+          line.push({ row: j, col: size - 1 - j })
+        }
+        return line
+      }
+    }
+  }
+
+  return []
+}
+
+module.exports = app
